5cdeffbae7a7d49c97291f6b8a0d9fce070fe89e
Fardin7864 <fardinhasan1888@gmail.com>
2023-12-12 16:43:47 +0600

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -219,6 +219,7 @@ function App() {
             </div>
           </div>
         </section>
+        {/* project section */}
         <section
           id="projects"
           className={activeSection === "projects" ? "active " : ""}
@@ -351,25 +352,28 @@ function App() {
                 </div>
                 <div className=" w-1/2 text-p">
                   <h4 className=" text-[#0099ff] text-2xl font-bold text-center my-3">
-                  E-Sports Organizer{" "}
+                    E-Sports Organizer{" "}
                   </h4>
                   <p>
-                  • Event Types: Anubis caters to diverse esports event types, ensuring a personalized and smooth experience.
-• Unique Design: The platform boasts a distinctive and responsive capturing the essence of gaming events.
-• Private Routes: To ensure the Safeguard sensitive information and authenticate users with private routes.
-• Local Storage: Enhance user experience by saving and retrieving preferences using local storage on client.
-• Responsive Design: Enjoy a seamlessly responsive layout that adapts to both desktop and mobile devices.
-• AOS Animation: For the Smooth and captivating animations using the AOS package on the homepage.
+                    • Event Types: Anubis caters to diverse esports event types,
+                    ensuring a personalized and smooth experience. • Unique
+                    Design: The platform boasts a distinctive and responsive
+                    capturing the essence of gaming events. • Private Routes: To
+                    ensure the Safeguard sensitive information and authenticate
+                    users with private routes. • Local Storage: Enhance user
+                    experience by saving and retrieving preferences using local
+                    storage on client. • Responsive Design: Enjoy a seamlessly
+                    responsive layout that adapts to both desktop and mobile
+                    devices. • AOS Animation: For the Smooth and captivating
+                    animations using the AOS package on the homepage.
                   </p>
                   <p className="my-2">
-                    <b>Technologies:</b> React.js, Tailwind, AOS, Framer Motion, Local Storage etc.
+                    <b>Technologies:</b> React.js, Tailwind, AOS, Framer Motion,
+                    Local Storage etc.
                   </p>
                   <div className=" flex justify-center items-center gap-9">
                     <button className=" text-blue-400 underline text-xl gap-5 flex justify-center items-center">
-                      <a
-                        href="https://esports-event.web.app/"
-                        target="_blank"
-                      >
+                      <a href="https://esports-event.web.app/" target="_blank">
                         Live
                       </a>
                     </button>
@@ -388,6 +392,72 @@ function App() {
               </div>
             </div>
           </div>
+        </section>
+        {/* Contact Section */}
+        <section
+          id="contact"
+          className={activeSection === "contact" ? "active " : ""}
+        >
+          <div className=" py-28 flex justify-center flex-col gap-8 items-center">
+            <h4 className=" text-[#FFEED9] text-3xl font-bold  my-3 border-b-2 border-blue-500">
+              Contact{" "}
+            </h4>
+            <div className=" bg-[#00008b] flex flex-col  items-center justify-center gap-3 py-8 px-36 rounded-lg">
+              <h4 className=" text-[#FFEED9] text-xl font-bold  my-3 ">
+                You can contact me via email:{" "}
+                <a
+                  href="mailto:fardinhasan18@gmail.com"
+                  className=" text-blue-500"
+                >
+                  fardinhasan18@gmail.com
+                </a>
+              </h4>
+              <h4 className=" text-[#FFEED9] text-base font-bold  my-3 ">
+                Or click the button below to send me a message:
+              </h4>
+
+              <div className=" flex gap-8 justify-center items-center">
+                <a href="mailto:fardinhasan18@gmail.com">
+                  <img
+                    src="https://img.shields.io/badge/Contact%20Me-Email-blue?style=for-the-badge&logo=gmail"
+                    alt="Contact Me"
+                  />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/fardinhasan"
+                  target="_blank"
+                >
+                  <img
+                    src="https://img.shields.io/badge/Connect%20with%20Me-LinkedIn-blue?style=for-the-badge&logo=linkedin"
+                    alt="Connect with Me on LinkedIn"
+                  />
+                </a>
+              </div>
+              <p className=" text-p">
+                Let's code something amazing together! 🚀
+              </p>
+            </div>
+          </div>
+        {/* Footer  */}
+        <footer className="footer items-center p-4 bg-neutral text-neutral-content justify-center" style={{backgroundColor: 'darkblue'}}>
+          <aside className="items-center grid-flow-col">
+            <svg
+              width="36"
+              height="36"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              fillRule="evenodd"
+              clipRule="evenodd"
+              className="fill-current"
+            >
+              <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
+            </svg>
+            <p>Copyright © 2023 - All right reserved</p>
+          </aside>
+          <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            {buttons}
+          </nav>
+        </footer>
         </section>
       </div>
     </div>
